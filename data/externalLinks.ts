db95eea29bdd2ded8719def963db466898439ad9
anthony-massaad <massaadanthony@hotmail.com>
2024-06-22 10:05:34 -0400

    update resume things

--- data/externalLinks.ts
+++ data/externalLinks.ts
@@ -1,6 +1,6 @@
 export const externalLinks = {
   linkedIn: "https://www.linkedin.com/in/anthony-massaad-782b911b6/",
   github: "https://github.com/Anthony-Massaad",
-  resume: "/resume.pdf",
-  grades: "/grades.pdf",
+  resume: "/Anthony_Massaad_Resume_2024.pdf",
+  grades: "/Anthony_Massaad_Grades.pdf",
 } as const;
