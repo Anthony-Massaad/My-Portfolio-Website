537d9d753a62f6a58d332c5684e3061011e46cbc
anthony-massaad <massaadanthony@hotmail.com>
2025-10-20 21:31:55 -0400

    integrated eslint to remove deprecated lint.

--- app/api/sendEmail/route.ts
+++ app/api/sendEmail/route.ts
@@ -5,7 +5,7 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const toEmail = process.env.TO_EMAIL;
 
-export async function POST(req: any, res: any): Promise<any> {
+export async function POST(req: any): Promise<any> {
   const { email, subject, message } = await req.json();
   try {
     const data = await resend.emails.send({
---
+++ eslint.config.js
@@ -0,0 +1,65 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  {
+    files: ["**/*.ts", "**/*.js", "**/*.tsx", "**/*.jsx"],
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    // A single asterisk (`*`) is not enough to ignore a directory and its contents.
+    // Use `**` for all subdirectories and files.
+    ignores: [
+      // Ignore the entire `dist` directory
+      "dist/**",
+      // Ignore the `test` directory and all its contents
+      "test/**",
+      "node_modules/**",
+      ".next/**",
+    ],
+  },
+  // Optional: Add custom rules or overrides here
+  {
+    rules: {
+      // Example: Disallow non-null assertions if desired
+      // Overrides some @typescript-eslint rules
+      "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+      "@typescript-eslint/consistent-indexed-object-style": [
+        "error",
+        "index-signature",
+      ],
+
+      // Enforce consistent use of type imports and exports
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/consistent-type-exports": "error",
+
+      // Disallow unused variables, including those named with an underscore
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+
+      // Prevent non-null assertions (!), a sign of a potential type issue
+      "@typescript-eslint/no-non-null-assertion": "error",
+
+      // Prevent redundant type definitions
+      "@typescript-eslint/no-redundant-type-constituents": "error",
+
+      // Disable JavaScript-specific rules that conflict with or are better handled by TypeScript
+      "no-unused-vars": "off", // Handled by @typescript-eslint/no-unused-vars
+      "no-redeclare": "off", // Handled by @typescript-eslint/no-redeclare
+    },
+  }
+);
